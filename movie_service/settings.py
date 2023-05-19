c5c009a76166dca3df9bce814d521e40af48a3b6
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-05-19 20:22:33 +0300

    Added django-ckeditor

--- movie_service/settings.py
+++ movie_service/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "ckeditor",
+    "ckeditor_uploader",
     "movies",
     "user",
 ]
@@ -129,6 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
