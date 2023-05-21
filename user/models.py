0dbc0452d33d955641907bcb11a04d844081244f
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-05-21 17:56:22 +0300

    Added user profile

--- user/models.py
+++ user/models.py
@@ -1,3 +1,5 @@
+from PIL import Image
+from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext as _
 from django.contrib.auth.models import AbstractUser, BaseUserManager
@@ -47,3 +49,35 @@ class User(AbstractUser):
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    image = models.ImageField(
+        upload_to="user_profile/",
+        blank=True,
+        null=True,
+        default="user.png"
+    )
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user}"
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None
+    ):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
