9e8f0b0168da79776a1ac9a9c258548c8a05cd6d
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-05-21 17:57:50 +0300

    Added user profile to admin.py

--- user/admin.py
+++ user/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 
-from user.models import User
-
+from user.models import User, Profile
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
